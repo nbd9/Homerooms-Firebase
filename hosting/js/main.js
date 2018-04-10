dae071a82074f8b91d703072ac06ddda5397d471
Nick DeGroot <nbdegroot1@gmail.com>
2018-04-09 21:16:23 -0700

    Updated db rules and added beta sign up

---
+++ hosting/js/main.js
@@ -0,0 +1,45 @@
+function signUp() {
+    firebase.auth().createUserWithEmailAndPassword(email = $('#email').val(), password = $('#password').val())
+        .then(function (user) {
+            var uid = user.uid
+            var name = $('#first_name').val() + ' ' + $('#last_name').val()
+            firebase.database().ref('/users/' + uid).set({
+                lastRequest: 0,
+                name: name,
+                seminars: {
+                    a: $("#teacher-a").val(),
+                    b: $("#teacher-b").val()
+                }
+            })
+
+            // firebase.auth().signOut().then(function () {
+            //     M.toast({ html: `Thank you! ${name} has been successfully registered for Homeroom.` })
+            // })
+        })
+        .catch(function (error) {
+            console.error(error)
+            M.toast({ html: error.message })
+        })
+}
+
+function checkForm() {
+    return $('#email').val().split('@').slice(1) === 'loswego.k12.or.us' && $('#password').val() && $('#first_name').val() && $('#last_name').val() && $("#teacher-a").val() && $("#teacher-b").val()
+}
+
+$(document).ready(function () {
+    $('select').formSelect();
+});
+
+firebase.database().ref('/teachers/').orderByChild('lastName').once('value').then(function (snapshot) {
+    var teachers = snapshot.val()
+
+    for (var key in teachers) {
+        if (teachers.hasOwnProperty(key)) {
+            var teacher = teachers[key]
+            var value = teacher.firstName + ' ' + teacher.lastName
+            $('select').append($("<option></option>").attr("value", teacher.id).text(value))
+        }
+    }
+
+    $('select').formSelect();
+})
